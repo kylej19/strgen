9f3c2d4447e885aacc9d55659dbc8dba2553bc01
kylej19 <kmj19@tuta.io>
2026-05-27 00:36:04 -0500

    counts, exclusions, and delimiters

--- src/generator.c
+++ src/generator.c
@@ -2,18 +2,19 @@
  * a password generator
  * written by kyle j
  */
-#include <unistd.h>
-#include <string.h>
+
+
 #include <ctype.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/random.h>
-
+#include <unistd.h>
 
 char *generate_buffer(size_t);
 void free_buffer(char *);
-unsigned char select_char(char *, size_t, int);
+unsigned char select_char(char *, size_t, int, char *);
 
 typedef enum {
   CLASS_LOWER = 1 << 0, /* 0001 */
@@ -28,24 +29,24 @@ int main(int argc, char **argv) {
 
   size_t password_length;
   CharClass enabled_classes[4];
-  int num_classes = 0;
-  int enabled = 0;
-
-
-  int opt;
-  // we're going to use getopt to parse our command-line arguments
-  // part of unistd.h
-  // int getopt (int ARGC, char *const *ARGV, const char *OPTIONS)  
-  while ((opt = getopt(argc, argv, "l:c:d:g:w:x:i")) != -1) {
+  int num_classes = 0, enabled = 0, password_count = 1, opt;
+  char *delimiter = "\n", *excluded = "";
+  
+  /* we're going to use getopt to parse our command-line arguments
+	 part of unistd.h
+	 int getopt (int ARGC, char *const *ARGV, const char *OPTIONS)  */
+  while ((opt = getopt(argc, argv, "l:c:d:g:x:i")) != -1) {
 	switch (opt) {
-	case 'l':
-	  // int atoi (const char *STRING)
-	  // parses int from optarg (set by getopt) and converts to size_t
+	case 'l': /* length */
+	  /* int atoi (const char *STRING)
+		 parses int from optarg (set by getopt) and converts to size_t */
 	  password_length = (size_t) atoi(optarg);
 	  break;
-	case 'c':
+	case 'c': /* count of strings to generate */
+	  password_count = atoi(optarg);
 	  break;
-	case 'd':
+	case 'd': /* change the delimiter from the default newline character*/
+	  delimiter = optarg;
 	  break;
 	case 'g':
 	  // we need to tokenize the optarg and use bitmasking to enable our character classes
@@ -61,11 +62,10 @@ int main(int argc, char **argv) {
 		  token = strtok(NULL,",");
 		}
 	  break;
-	case 'w':
+	case 'x': /* explicitly excluded characters */
+	  excluded = optarg;
 	  break;
-	case 'x':
-	  break;
-	case 'i':
+	case 'i': /* prints info about generated passwords */
 	  break;
 	}
   }
@@ -74,27 +74,33 @@ int main(int argc, char **argv) {
   if (enabled & CLASS_DIGIT) enabled_classes[num_classes++] = CLASS_DIGIT;
   if (enabled & CLASS_PUNCT) enabled_classes[num_classes++] = CLASS_PUNCT;
 
-  char password[password_length];
- 
-  for (int i = 0; i < (int) password_length; i++) {
-	CharClass current = enabled_classes[i % num_classes]; // cycles 0,1,2,0,1,2...
-	char *buffer = generate_buffer(BUF_SIZE);
-	password[i] = select_char(buffer, BUF_SIZE, current); // single-bit mask
-	free_buffer(buffer);
-  }
+  for (int count = 0; count < password_count; count++) {
+	/* print the delimiter on the second or more */
+	if (count > 0)
+	  printf("%s", delimiter);
+	/* buffer to store the generated characters */
+	char password[password_length];
+	/* generate the characters and select them with enabled classes */
+	for (int i = 0; i < (int) password_length; i++) {
+	  CharClass current = enabled_classes[i % num_classes]; /* cycles 0,1,2,0,1,2... */
+	  char *buffer = generate_buffer(BUF_SIZE);
+	  password[i] = select_char(buffer, BUF_SIZE, current, excluded); /* single-bit mask */
+	  free_buffer(buffer);
+	}
+	/* then shuffle using Fisher-Yates */
+	for (int i = password_length - 1; i > 0; i--) {
+	  /* need a random index from 0..i */
+	  unsigned char rand_byte;
+	  getrandom(&rand_byte, 1, 0);
+	  int j = rand_byte % (i + 1);
+	  char tmp = password[i];
+	  password[i] = password[j];
+	  password[j] = tmp;
+	}
+	for (int i = 0; i < (int) password_length; i++) {
+	  printf("%c",password[i]);
+	}
 
-  // then shuffle using Fisher-Yates
-  for (int i = password_length - 1; i > 0; i--) {
-    // need a random index from 0..i
-    unsigned char rand_byte;
-    getrandom(&rand_byte, 1, 0);
-    int j = rand_byte % (i + 1);
-    char tmp = password[i];
-    password[i] = password[j];
-    password[j] = tmp;
-  }
-  for (int i = 0; i < password_length; i++) {
-	printf("%c",password[i]);
   }
 
   printf("\n"); 
@@ -119,13 +125,16 @@ void free_buffer(char *buffer) {
 	printf("buffer is NULL!\n");
 }
 
-unsigned char select_char(char *buffer, size_t len, int class) {
+unsigned char select_char(char *buffer, size_t len, int class, char *excluded) {
   if (!buffer)
 	return '\0';
-  // loop through the buffer to find a valid char
+  /* loop through the buffer to find a valid char */
   for (int i = 0; i < (int) len; i++) {
-	// cast the character being tested with a mask (just to be sure)
+	/* cast the character being tested with a mask (just to be sure) */
 	unsigned char c = *(buffer + i) & 0x7f;
+	/* check against the exclusion list */
+	if (excluded && strchr(excluded, c))
+	  continue;
 	if ((class & CLASS_LOWER) && islower(c)) return c;
     if ((class & CLASS_UPPER) && isupper(c)) return c;
     if ((class & CLASS_DIGIT) && isdigit(c)) return c;
